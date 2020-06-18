efa285efbcb8635f23448df94a24d8037a5991fd
Oscar Jin <kintininn@gmail.com>
2020-06-18 10:28:45 +0900

    set link target to blank

--- src/page/BugsAndIssuesPage.js
+++ src/page/BugsAndIssuesPage.js
@@ -3,7 +3,11 @@ import React from "react";
 const BugsAndIssuesPage = () => {
   return (
     <div>
-      <a href="https://www.dropbox.com/s/kuvs43juptes0i5/Screen%20Shot%202020-06-18%20at%205.51.49.png?dl=0">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.dropbox.com/s/kuvs43juptes0i5/Screen%20Shot%202020-06-18%20at%205.51.49.png?dl=0"
+      >
         サブスクリプション変更時に重複する可能性（原因未特定）
       </a>
     </div>
