967bb1a4039b1e6894287559057299f28434d120
Oscar Jin <kintininn@gmail.com>
2020-06-25 23:25:49 +0900

    add capacity 0

--- src/select/CapacitySelect.js
+++ src/select/CapacitySelect.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CapacitySelect = props => {
   const { value, onChange } = checkprops(props);
-  const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
     <select
