3a870127fe1da758e2c34f5820167ca4d9d99fa4
Oscar Jin <kintininn@gmail.com>
2020-07-08 03:48:13 +0900

    add style

--- src/module/ExecutiveSubscriptionsModule.js
+++ src/module/ExecutiveSubscriptionsModule.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectSubscriptions, selectStudents } from "../redux/selector";
+import {
+  selectSubscriptions,
+  selectStudents,
+  selectTickets,
+} from "../redux/selector";
 import moment from "moment";
 import { localizePlan } from "./StudentSubscriptionModule";
 import useFindLatestMembership from "../hook/useFindLatestMembership";
@@ -8,6 +12,7 @@ import { localizeStatus } from "../toolkit/localize";
 import "../style/_executiveSubscriptionsModule.scss";
 import { Link } from "react-router-dom";
 import { student$subscription } from "../page/StudentPage";
+import { $type } from "../template/ticket";
 
 const nowIso8601 = moment().date(1).format("YYYY-MM-DD");
 const nextIso8601 = moment().add(1, "month").date(1).format("YYYY-MM-DD");
@@ -15,14 +20,23 @@ const nextIso8601 = moment().add(1, "month").date(1).format("YYYY-MM-DD");
 const ExecutiveSubscriptionsModule = () => {
   const findLatestMembership = useFindLatestMembership();
 
+  const tickets = useSelector(selectTickets);
   const students = useSelector(selectStudents);
   const subscriptions = useSelector(selectSubscriptions);
   const thisMonths = subscriptions?.filter(S => S.iso8601 === nowIso8601);
   const nextMonths = subscriptions?.filter(S => S.iso8601 === nextIso8601);
+  const purchasedThisMonth = tickets?.filter(
+    T => T.iso8601 === nowIso8601 && T.type === $type.singlePurchase
+  );
+  const purchasedNextMonth = tickets?.filter(
+    T => T.iso8601 === nextIso8601 && T.type === $type.singlePurchase
+  );
 
   const tableRows = students?.map(St => {
     const thisMonthsPlan = thisMonths?.find(S => S.uid === St.id)?.plan;
     const nextMonthsPlan = nextMonths?.find(S => S.uid === St.id)?.plan;
+    const thisMonthsPurchase = purchasedThisMonth?.filter(T => T.uid === St.id);
+    const nextMonthsPurchase = purchasedNextMonth?.filter(T => T.uid === St.id);
     const latestStatus = findLatestMembership(St.id)?.status;
     const hasPlanChange =
       nextMonthsPlan !== undefined && thisMonthsPlan !== nextMonthsPlan;
@@ -37,11 +51,23 @@ const ExecutiveSubscriptionsModule = () => {
         <td className={latestStatus}>
           {localizeStatus(latestStatus, "short")}
         </td>
-        <td style={hasPlanChange ? indicator : {}}>
+        <td
+          className={thisMonthsPlan ? "" : "no-data"}
+          style={hasPlanChange ? indicator : {}}
+        >
           {localizePlan(thisMonthsPlan)}
         </td>
-        <td style={hasPlanChange ? indicator : {}}>
+        <td style={thisMonthsPurchase?.length ? indicator : {}}>
+          {thisMonthsPurchase?.length}
+        </td>
+        <td
+          className={nextMonthsPlan ? "" : "no-data"}
+          style={hasPlanChange ? indicator : {}}
+        >
           {localizePlan(nextMonthsPlan)}
+        </td>
+        <td style={nextMonthsPurchase?.length ? indicator : {}}>
+          {nextMonthsPurchase?.length}
         </td>
       </tr>
     );
@@ -55,7 +81,9 @@ const ExecutiveSubscriptionsModule = () => {
             <th>生徒</th>
             <th>ステータス</th>
             <th>{moment().month() + 1}月</th>
+            <th>補講</th>
             <th>{moment().add(1, "month").month() + 1}月</th>
+            <th>補講</th>
           </tr>
         </thead>
         <tbody>{tableRows}</tbody>
